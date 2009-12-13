23956009cfd800b1e52614e1e2a502cf375f8143
m.dornseif@hudora.de <m.dornseif@hudora.de@2a7d6d32-97f8-0310-8fd3-c67d3a265405>
2009-12-13 23:03:19 +0000

    .
    git-svn-id: https://cybernetics.hudora.biz/intern/svn/code/projects/huBarcode/trunk@7374 2a7d6d32-97f8-0310-8fd3-c67d3a265405

--- barcodeserver/settings_live.py
+++ barcodeserver/settings_live.py
@@ -4,9 +4,9 @@
 from settings import *
 
 DATABASE_ENGINE = 'postgresql_psycopg2' # 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
-DATABASE_NAME = 'hudora'       # Or path to database file if using sqlite3.
-DATABASE_USER = 'hudora'       # Not used with sqlite3. # webserver
-DATABASE_PASSWORD = 'ge3Xei2O' # Not used with sqlite3. # zimZity
-DATABASE_HOST = '172.30.13.20' # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_NAME = 'dbname'       # Or path to database file if using sqlite3.
+DATABASE_USER = 'user'         # Not used with sqlite3. # webserver
+DATABASE_PASSWORD = 'geheim'   # Not used with sqlite3. # zimZity
+DATABASE_HOST = '0.0.0.0'      # Set to empty string for localhost. Not used with sqlite3.
 
 DEBUG = False
